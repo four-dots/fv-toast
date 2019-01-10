34ebcdc5340ae3c7f5f30ee55a6d13cbe753daec
Kristijan Novaković <kristijan.novakovic@fourdots.co>
2019-01-10 17:44:46 +0100

    [update]

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,7 +1,12 @@
 <template>
-    <section class="bv-accordion" :data-id="id" :data-state="state">
+    <section
+        class="bv-accordion"
+        :class="{'bv-accordion--without-toggle': !togglable}"
+        :data-id="id"
+        :data-state="state"
+    >
         <header class="bv-accordion__header">
-            <div class="bv-accordion__toggle" @click="toggle">
+            <div v-if="togglable" class="bv-accordion__toggle" @click="toggle">
                 <template v-if="usesFontAwesome">
                     <span class="icon"><i class="fas fal" :class="[state ? 'fa-minus' : 'fa-plus']"></i></span>
                 </template>
@@ -26,9 +31,16 @@ import nanoid from 'nanoid';
 export default {
     props: {
         usesFontAwesome: {
-            default: true,
             type: Boolean,
-            open: false,
+            default: true,
+        },
+        togglable: {
+            type: Boolean,
+            default: true,
+        },
+        open: {
+            type: Boolean,
+            default: false,
         },
     },
     data() {
@@ -39,10 +51,12 @@ export default {
     },
     methods: {
         toggle() {
+            if (!this.togglable) return;
             this.state = !this.state;
             this.$emit('toggle', this.state);
         },
         toggleFromOutside(value) {
+            if (!this.togglable) return;
             this.state = value;
             this.$emit('toggle', this.state);
         },
@@ -69,22 +83,31 @@ export default {
     flex-grow: 0;
     flex-shrink: 0;
     padding-right: 0.5rem;
-    cursor: pointer;
     font-size: 0.75rem;
+
+    .bv-accordion:not(.bv-accordion--without-toggle) & {
+        cursor: pointer;
+    }
 }
 .bv-accordion__actions {
     flex-grow: 1;
     flex-shrink: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 }
 .bv-accordion__title {
     flex-basis: 100%;
     flex-grow: 2;
     flex-shrink: 1;
-    cursor: pointer;
 
     .title {
         margin: 0;
         line-height: 1;
+    }
+
+    .bv-accordion:not(.bv-accordion--without-toggle) & {
+        cursor: pointer;
     }
 }
 .bv-accordion__content {
