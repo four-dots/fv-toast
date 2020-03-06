a98336cc5af7ec135f4f622ce42f4a0458485e5e
Kristijan <eldair.k@gmail.com>
2020-03-06 17:10:59 +0100

    v1.1.22

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,39 +1,42 @@
-import vue from "rollup-plugin-vue"; // Handle .vue SFC files
-import commonjs from "rollup-plugin-commonjs";
-import css from "rollup-plugin-css-only";
-import babel from "rollup-plugin-babel";
+import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+import commonjs from 'rollup-plugin-commonjs';
+import css from 'rollup-plugin-css-only';
+import babel from 'rollup-plugin-babel';
 
 export default {
-    input: "src/index.js", // Path relative to package.json
-    external: ["nanoid"],
+    input: 'src/index.js', // Path relative to package.json
+    external: ['nanoid'],
     output: [
         {
-            name: "BulmaVueAccordion",
-            exports: "named",
-            format: "umd",
-            file: "dist/index.umd.js"
+            name: 'BulmaVueAccordion',
+            exports: 'named',
+            format: 'umd',
+            file: 'dist/index.umd.js',
         },
         {
-            name: "BulmaVueAccordion",
-            exports: "named",
-            format: "es",
-            file: "dist/index.esm.js"
+            name: 'BulmaVueAccordion',
+            exports: 'named',
+            format: 'es',
+            file: 'dist/index.esm.js',
         },
         {
-            name: "BulmaVueAccordion",
-            exports: "named",
-            format: "iife",
-            file: "dist/index.min.js"
-        }
+            name: 'BulmaVueAccordion',
+            exports: 'named',
+            format: 'iife',
+            file: 'dist/index.min.js',
+        },
     ],
     plugins: [
+        css({
+            output: 'dist/styles.css',
+        }),
         vue({
-            css: true, // Dynamically inject css as a <style> tag
-            compileTemplate: true // Explicitly convert template to render function
+            css: false, // Dynamically inject css as a <style> tag
+            compileTemplate: true, // Explicitly convert template to render function
         }),
         commonjs(),
         babel({
-            exclude: "node_modules/**"
-        })
-    ]
+            exclude: 'node_modules/**',
+        }),
+    ],
 };
