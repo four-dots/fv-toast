e567f32538eda53eaed9fc65d46b00ad3f71e163
Kristijan Novaković <eldair.k@gmail.com>
2021-10-22 14:04:33 +0200

    Build

--- dist/bulma-vue-accordion.es.js
+++ dist/bulma-vue-accordion.es.js
@@ -1,2 +1,206 @@
-import{provide as e,onMounted as t,onBeforeUnmount as o,openBlock as l,createElementBlock as n,renderSlot as a,ref as s,inject as i,watch as c,nextTick as d,normalizeClass as r,createElementVNode as g,Fragment as u,createTextVNode as h,toDisplayString as f,createCommentVNode as p,createVNode as v,Transition as y,withCtx as m,withDirectives as b,vShow as _}from"vue";import w from"mitt";import{nanoid as C}from"nanoid";const k=w(),A={name:"bv-accordions",props:{single:{default:!0,type:Boolean}},setup(l,{slots:n}){const a=[],s=n.default()[0].children.length,i=e=>{let t=a.reduce(((t,o)=>o.id===e?t:(o.state=!1,[o.id,...t])),[]);k.emit("accordion.close",t)},c=({accordionId:e,state:t})=>{l.single&&t&&a.find((t=>t.id===e))&&i(e)};e("registerChild",(e=>{a.push(e),a.length===s&&(()=>{if(!l.single)return;let e=null;for(const t of a)if(e||(e=t.state?t.id:null),e)break;e&&i(e)})()})),t((()=>{k.on("accordion.toggle",c)})),o((()=>{k.off("accordion.toggle")}))}},E={class:"bv-accordions"};A.render=function(e,t,o,s,i,c){return l(),n("section",E,[a(e.$slots,"default")])};const S={name:"bv-accordion",props:{usesFontAwesome:{type:Boolean,default:!0},togglable:{type:Boolean,default:!0},open:{type:Boolean,default:!1}},emits:["toggle"],setup(e,{emit:t}){const l=s(e.open),n=C(),a=i("registerChild",null),r=async()=>{e.togglable&&(await d(),l.value=!l.value,t("toggle",l.value),k.emit("accordion.toggle",{accordionId:n,state:l.value}))};return k.on("accordion.close",(t=>{l.value&&e.togglable&&t.includes(n)&&(l.value=!1)})),a&&a({id:n,state:l.value}),o((()=>k.off("accordion.close"))),c((()=>e.open),(e=>{e!==l.value&&r()})),{accordionId:n,state:l,toggle:r,enter:async e=>{const t=getComputedStyle(e).width;e.style.width=t,e.style.position="absolute",e.style.visibility="hidden",e.style.height="auto";const o=getComputedStyle(e).height;e.style.width=null,e.style.position=null,e.style.visibility=null,e.style.height=0,getComputedStyle(e).height,await d(),e.style.height=o},afterEnter:e=>{e.style.height="auto"},leave:async e=>{const t=getComputedStyle(e).height;e.style.height=t,getComputedStyle(e).height,await d(),e.style.height=0}}}},B=["data-accordion-id"],I={class:"bv-accordion__header"},$={key:0,class:"icon"},F={class:"bv-accordion__actions"},L={class:"bv-accordion__content content"};S.render=function(e,t,o,s,i,c){return l(),n("section",{class:r(["bv-accordion",{"bv-accordion--without-toggle":!o.togglable,"is-open":s.state}]),"data-accordion-id":s.accordionId},[g("header",I,[o.togglable?(l(),n("div",{key:0,class:"bv-accordion__toggle",onClick:t[0]||(t[0]=(...e)=>s.toggle&&s.toggle(...e))},[o.usesFontAwesome?(l(),n("span",$,[g("i",{class:r(["fas fal",[s.state?"fa-minus":"fa-plus"]])},null,2)])):(l(),n(u,{key:1},[h(f(s.state?"-":"+"),1)],64))])):p("",!0),g("div",{class:"bv-accordion__title",onClick:t[1]||(t[1]=(...e)=>s.toggle&&s.toggle(...e))},[a(e.$slots,"title")]),g("div",F,[a(e.$slots,"actions")])]),v(y,{name:"bv-accordion-toggle",onEnter:s.enter,onAfterEnter:s.afterEnter,onLeave:s.leave},{default:m((()=>[b(g("article",L,[a(e.$slots,"content",{state:s.state})],512),[[_,s.state]])])),_:3},8,["onEnter","onAfterEnter","onLeave"])],10,B)};var x=e=>{e.component("Accordion",S),e.component("Accordions",A)};export{x as default};
+import { provide, onMounted, onBeforeUnmount, openBlock, createElementBlock, renderSlot, ref, inject, watch, normalizeClass, createElementVNode, Fragment, createTextVNode, toDisplayString, createCommentVNode, createVNode, Transition, withCtx, withDirectives, vShow, nextTick } from "vue";
+import mitt from "mitt";
+import { nanoid } from "nanoid";
+const emitter = mitt();
+var _export_sfc = (sfc, props) => {
+  for (const [key, val] of props) {
+    sfc[key] = val;
+  }
+  return sfc;
+};
+const _sfc_main$1 = {
+  name: "bv-accordions",
+  props: {
+    single: {
+      default: true,
+      type: Boolean
+    }
+  },
+  setup(props, { slots }) {
+    const children = [];
+    const registeredAccordions = slots.default()[0].children.length;
+    const dispatchCloseEvent = (accordionId) => {
+      let accordionsToClose = children.reduce((carry, child) => {
+        if (child.id === accordionId)
+          return carry;
+        child.state = false;
+        return [child.id, ...carry];
+      }, []);
+      emitter.emit("accordion.close", accordionsToClose);
+    };
+    const closeAccordions = ({ accordionId, state }) => {
+      if (!props.single || !state)
+        return;
+      if (!children.find((child) => child.id === accordionId))
+        return;
+      dispatchCloseEvent(accordionId);
+    };
+    const closeOtherAccordions = () => {
+      if (!props.single)
+        return;
+      let openAccordion = null;
+      for (const child of children) {
+        openAccordion || (openAccordion = child.state ? child.id : null);
+        if (openAccordion)
+          break;
+      }
+      if (!openAccordion)
+        return;
+      dispatchCloseEvent(openAccordion);
+    };
+    const registerChild = (child) => {
+      children.push(child);
+      if (children.length !== registeredAccordions)
+        return;
+      closeOtherAccordions();
+    };
+    provide("registerChild", registerChild);
+    onMounted(() => {
+      emitter.on("accordion.toggle", closeAccordions);
+    });
+    onBeforeUnmount(() => {
+      emitter.off("accordion.toggle");
+    });
+  }
+};
+const _hoisted_1$1 = { class: "bv-accordions" };
+function _sfc_render$1(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("section", _hoisted_1$1, [
+    renderSlot(_ctx.$slots, "default")
+  ]);
+}
+var Accordions = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render$1]]);
+const _sfc_main = {
+  name: "bv-accordion",
+  props: {
+    usesFontAwesome: {
+      type: Boolean,
+      default: true
+    },
+    togglable: {
+      type: Boolean,
+      default: true
+    },
+    open: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ["toggle"],
+  setup(props, { emit }) {
+    const state = ref(props.open);
+    const accordionId = nanoid();
+    const registerChild = inject("registerChild", null);
+    const toggle = async (newState) => {
+      if (!props.togglable)
+        return;
+      await nextTick();
+      state.value = newState !== void 0 ? newState : !state.value;
+      emit("toggle", state.value);
+      emitter.emit("accordion.toggle", { accordionId, state: state.value });
+    };
+    const enter = async (element) => {
+      const width = getComputedStyle(element).width;
+      element.style.width = width;
+      element.style.position = "absolute";
+      element.style.visibility = "hidden";
+      element.style.height = "auto";
+      const height = getComputedStyle(element).height;
+      element.style.width = null;
+      element.style.position = null;
+      element.style.visibility = null;
+      element.style.height = 0;
+      getComputedStyle(element).height;
+      await nextTick();
+      element.style.height = height;
+    };
+    const afterEnter = (element) => {
+      element.style.height = "auto";
+    };
+    const leave = async (element) => {
+      const height = getComputedStyle(element).height;
+      element.style.height = height;
+      getComputedStyle(element).height;
+      await nextTick();
+      element.style.height = 0;
+    };
+    const close = (accordionsToClose) => {
+      if (!state.value || !props.togglable || !accordionsToClose.includes(accordionId))
+        return;
+      state.value = false;
+    };
+    emitter.on("accordion.close", close);
+    if (registerChild) {
+      registerChild({ id: accordionId, state: state.value });
+    }
+    onBeforeUnmount(() => emitter.off("accordion.close"));
+    watch(() => props.open, (value) => {
+      if (value === state.value)
+        return;
+      toggle();
+    });
+    return { accordionId, state, toggle, enter, afterEnter, leave };
+  }
+};
+const _hoisted_1 = ["data-accordion-id"];
+const _hoisted_2 = { class: "bv-accordion__header" };
+const _hoisted_3 = {
+  key: 0,
+  class: "icon"
+};
+const _hoisted_4 = { class: "bv-accordion__actions" };
+const _hoisted_5 = { class: "bv-accordion__content content" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("section", {
+    class: normalizeClass(["bv-accordion", { "bv-accordion--without-toggle": !$props.togglable, "is-open": $setup.state }]),
+    "data-accordion-id": $setup.accordionId
+  }, [
+    createElementVNode("header", _hoisted_2, [
+      $props.togglable ? (openBlock(), createElementBlock("div", {
+        key: 0,
+        class: "bv-accordion__toggle",
+        onClick: _cache[0] || (_cache[0] = (...args) => $setup.toggle && $setup.toggle(...args))
+      }, [
+        $props.usesFontAwesome ? (openBlock(), createElementBlock("span", _hoisted_3, [
+          createElementVNode("i", {
+            class: normalizeClass(["fas fal", [$setup.state ? "fa-minus" : "fa-plus"]])
+          }, null, 2)
+        ])) : (openBlock(), createElementBlock(Fragment, { key: 1 }, [
+          createTextVNode(toDisplayString($setup.state ? "-" : "+"), 1)
+        ], 64))
+      ])) : createCommentVNode("", true),
+      createElementVNode("div", {
+        class: "bv-accordion__title",
+        onClick: _cache[1] || (_cache[1] = (...args) => $setup.toggle && $setup.toggle(...args))
+      }, [
+        renderSlot(_ctx.$slots, "title")
+      ]),
+      createElementVNode("div", _hoisted_4, [
+        renderSlot(_ctx.$slots, "actions")
+      ])
+    ]),
+    createVNode(Transition, {
+      name: "bv-accordion-toggle",
+      onEnter: $setup.enter,
+      onAfterEnter: $setup.afterEnter,
+      onLeave: $setup.leave
+    }, {
+      default: withCtx(() => [
+        withDirectives(createElementVNode("article", _hoisted_5, [
+          renderSlot(_ctx.$slots, "content", { state: $setup.state })
+        ], 512), [
+          [vShow, $setup.state]
+        ])
+      ]),
+      _: 3
+    }, 8, ["onEnter", "onAfterEnter", "onLeave"])
+  ], 10, _hoisted_1);
+}
+var Accordion = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+var style = "";
+var index = (app) => {
+  app.component("Accordion", Accordion);
+  app.component("Accordions", Accordions);
+};
+export { index as default };
 //# sourceMappingURL=bulma-vue-accordion.es.js.map
